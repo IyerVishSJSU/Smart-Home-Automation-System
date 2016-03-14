1b076f1f2bfba3a34966cae83be1f8c978f9f1d4
Vishwanath Iyer <Vishwanath Iyer>
2016-03-13 20:34:27 -0700

    First Commit

---
+++ client2/tcpclient.c
@@ -0,0 +1,358 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <stdbool.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <time.h>
+#include<pthread.h>
+#define TIME_TO_PUSH 30
+
+/*for getting file size using stat()*/
+#include<sys/stat.h>
+
+/*for sendfile()*/
+#include<sys/sendfile.h>
+
+/*for O_RDONLY*/
+#include<fcntl.h>
+
+int gasValue = 0;
+int electricityValue = 0;
+int waterValue = 0;
+int clientId =1002;
+char receivebuffer[256],sendbuffer[256];
+int csock;
+
+
+void push_to_client(){
+
+	//code to push to client
+	char *pts;			/* pointer to time string	*/
+	time_t	now;			/* current time			*/
+	char	*ctime();
+
+	while(1){
+
+	sleep(TIME_TO_PUSH);
+
+	(void) time(&now);
+	pts = ctime(&now);
+
+	strftime(pts,strlen(pts),"%m/%d/%Y %H:%M:%S %a",localtime(&now));
+	sprintf(sendbuffer,"%s%c%d%c%d%c%d%c%s%c%d","usage",'|',waterValue,'|',gasValue,'|',electricityValue,'|',pts,'|',clientId);
+
+	send(csock, sendbuffer,sizeof(sendbuffer),0);
+	bzero(receivebuffer,sizeof(receivebuffer));
+	recv(csock, &receivebuffer,sizeof(receivebuffer),0);
+	gasValue = 0;
+	electricityValue = 0;
+	waterValue = 0;
+	printf("%s\n",receivebuffer);
+
+	}
+}
+
+int main(int argc, char *argv[]){
+
+	int port,conflag,size;
+	char *fileptr;
+	char filename[20];
+	struct sockaddr_in server_addr;
+	struct hostent *server_name;
+	int choice,choice1,status;
+	int filehandle;
+	int i = 1;
+
+	if(argc < 3){
+		printf("Please enter in the form of \"./tcpclient host_IP(localhost) port_number\" \n");
+		exit(-1);
+	}
+
+	csock=socket(AF_INET,SOCK_STREAM,0);
+
+	if(csock==-1){
+		printf("socket failure\n");
+		exit(-1);
+	}
+
+	server_name=gethostbyname(argv[1]);
+
+	if(server_name==NULL){
+		printf("No host found\n");
+		exit(-1);
+	}
+
+	port=atoi(argv[2]);
+
+	bzero((char *)&server_addr, sizeof(server_addr));
+	server_addr.sin_family=AF_INET;
+	server_addr.sin_port=htons(port);
+	bcopy((char *)server_name->h_addr,(char *)&server_addr.sin_addr.s_addr,server_name->h_length);
+
+	conflag=connect(csock,(struct sockaddr *)&server_addr,sizeof(server_addr));
+
+	if(conflag<0){
+		printf("Error in connecting to Server...\n");
+		exit(-1);
+	}
+
+	/*int timer = 0;*/
+
+	/*------Test Start------*/
+	pthread_attr_t ta;
+	pthread_t th;
+	(void) pthread_attr_init(&ta);
+	(void) pthread_attr_setdetachstate(&ta, PTHREAD_CREATE_DETACHED);
+	if (pthread_create(&th, &ta, (void * (*)(void *))push_to_client, NULL) < 0)
+		printf("Thread error \n");
+
+	while(1){
+
+		printf("Enter a choice:\n1- Activities\n2- Generate a Report\n3- quit\n");
+
+		scanf("%d",&choice);
+
+		switch(choice)
+		{
+			case 1:
+
+			 printf("Activities\n");
+
+			 while(1)
+			 {
+
+			  printf("Enter a choice:\n1- Bathing\n2- Washing Machine\n3- Dish Washing\n4- Lighting\n5- Air Conditioner\n"
+					  "6- Electronic Devices\n7- Cooking\n8- Water Heater\n9- Back\n");
+			  scanf("%d",&choice1);
+
+			  int test=0;
+
+			  switch(choice1)
+				{
+
+				case 1:
+
+				  printf("Bathing Used\n");
+				  waterValue = waterValue + 30;
+
+				  break;
+
+				case 2:
+
+				  printf("Washing Used\n");
+				  waterValue = waterValue + 10;
+
+				  break;
+
+				case 3:
+
+				  printf("Dish Washing Used\n");
+				  waterValue = waterValue + 20;
+
+				  break;
+
+				case 4:
+
+				  printf("Lighting Used\n");
+				  electricityValue = electricityValue + 60;
+
+				  break;
+
+				case 5:
+
+				  printf("Air Conditioner Used\n");
+				  electricityValue = electricityValue + 30;
+
+				  break;
+
+				case 6:
+
+				  printf("Electronic Devices Used\n");
+				  electricityValue = electricityValue + 10;
+
+				  break;
+
+				case 7:
+
+				  printf("Cooking Used\n");
+				  gasValue = gasValue + 40;
+
+				  break;
+
+				case 8:
+
+				  printf("Water Heater Used\n");
+				  gasValue = gasValue + 20;
+				  break;
+
+				case 9:
+
+				  printf("Back\n");
+				  test=1;
+				  break;
+
+				default:
+
+				  printf("Please Enter a Valid choice\n");
+				  break;
+
+				}
+
+			  if(test){
+				  test = 0;
+				  break;
+			  }
+
+			}
+
+			  break;
+
+			case 2:
+
+			  printf("Generate a Report\n");
+
+			  while(1)
+				 {
+
+				  printf("Enter a choice:\n1- Text Report\n2- Graph Report\n3- Back\n");
+				  scanf("%d",&choice1);
+
+				  int test=0;
+
+				  switch(choice1)
+					{
+
+				  	  case 1:
+
+				  	  sprintf(sendbuffer,"%s%c%d","text",'|',clientId);
+					  send(csock, sendbuffer, 100, 0);
+
+					  bzero(receivebuffer,sizeof(receivebuffer));
+					  recv(csock, receivebuffer, sizeof(receivebuffer), 0);
+
+					  strcpy(filename,receivebuffer);
+
+					  send(csock, receivebuffer, 100, 0);
+
+					  recv(csock, &size, sizeof(int), 0);
+
+					  if(!size)
+					  {
+						  printf("No such file on the remote directory\n\n");
+						  break;
+					  }
+
+					  fileptr = malloc(size);
+					  recv(csock, fileptr, size, 0);
+
+					  while(1)
+					  {
+						  filehandle = open(filename, O_CREAT | O_EXCL | O_WRONLY, 0666);
+						  if(filehandle == -1)
+						  {
+							  sprintf(filename + strlen(filename), "%d", i);//needed only if same directory is used for both server and client
+						  }
+						  else break;
+					  }
+
+					  write(filehandle, fileptr, size);
+					  close(filehandle);
+
+					  printf("The file %s has been downloaded\n",filename);
+
+					  break;
+
+				  	  case 2:
+
+					  sprintf(sendbuffer,"%s%c%d","graph",'|',clientId);
+					  send(csock, sendbuffer, 100, 0);
+
+					  bzero(receivebuffer,sizeof(receivebuffer));
+					  recv(csock, receivebuffer, sizeof(receivebuffer), 0);
+
+					  strcpy(filename,receivebuffer);
+
+					  send(csock, receivebuffer, 100, 0);
+
+					  recv(csock, &size, sizeof(int), 0);
+
+					  if(!size)
+					  {
+						  printf("No such file on the remote directory\n\n");
+						  break;
+					  }
+
+					  fileptr = malloc(size);
+					  recv(csock, fileptr, size, 0);
+
+					  while(1)
+					  {
+						  filehandle = open(filename, O_CREAT | O_EXCL | O_WRONLY, 0666);
+						  if(filehandle == -1)
+						  {
+							  sprintf(filename + strlen(filename), "%d", i);//needed only if same directory is used for both server and client
+						  }
+						  else break;
+					  }
+
+					  write(filehandle, fileptr, size);
+					  close(filehandle);
+
+					  printf("The file %s has been downloaded\n",filename);
+
+					  break;
+
+				  	  case 3:
+
+					  printf("Back\n");
+					  test = 1;
+					  break;
+
+				  	default: printf("Please Enter a Valid choice\n");
+				  			 break;
+
+					}
+
+					if(test){
+					  test = 0;
+					  break;
+					}
+				 }
+
+			  break;
+
+			case 3:
+
+			  strcpy(sendbuffer, "quit");
+			  send(csock, sendbuffer, 100, 0);
+			  recv(csock, &status, 100, 0);
+
+			  if(status){
+				  printf("Client is closed\nQuitting..\n");
+				  exit(0);
+			  }
+
+			  printf("Client failed to close connection\n");
+			  break;
+
+
+			default: printf("Please Enter a Valid choice\n");
+					 break;
+		}
+
+	}
+
+	pthread_cancel(th);
+
+	/*------Test End------*/
+
+	close(csock);
+
+	return 0;
+}
---
+++ server/tcpserver.c
@@ -0,0 +1,380 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<netinet/in.h>
+#include<string.h>
+#include<stdlib.h>
+#include<arpa/inet.h>
+#include<string.h>
+#include<errno.h>
+#include "gnuplot_i.h"
+#include<pthread.h>
+
+/*for getting file size using stat()*/
+#include<sys/stat.h>
+
+/*for sendfile()*/
+#include<sys/sendfile.h>
+
+/*for O_RDONLY*/
+#include<fcntl.h>
+
+void *client_req(void*);
+float conservation(int value,int threshold);
+float consumption(int value,int threshold);
+
+pthread_mutex_t usage_lock;
+
+int w_val = 100;
+int g_val = 80;
+int e_val = 120;
+char firstname[10]= {0};
+
+
+int main(int argc, char *argv[]){
+
+	int sd, bindval,csock;
+	struct sockaddr_in client_addr, server_addr;
+	socklen_t clilength;
+	int backlog=5;
+
+	fd_set rfds;
+    FD_ZERO(&rfds);
+    int nfds = getdtablesize();
+	pthread_t thread1;
+
+	if(argc < 2){
+		printf("Please enter in the form of \"./tcpserver port_number\" \n");
+		exit(-1);
+	}
+
+	sd=socket(PF_INET,SOCK_STREAM,0);
+
+	if(sd<0){
+		printf("socket failure\n");
+		exit(1);
+	}
+
+	bzero((char *) &server_addr, sizeof(server_addr));
+
+	server_addr.sin_family=AF_INET;
+	server_addr.sin_port=htons(atoi(argv[1]));
+	server_addr.sin_addr.s_addr=INADDR_ANY;
+	bindval=bind(sd,(struct sockaddr *) &server_addr,sizeof(server_addr));
+
+	if(bindval<0){
+		printf("Binding failure");
+		exit(1);
+	}
+	listen(sd,backlog);
+
+	clilength=sizeof(client_addr);
+
+    fflush(stdout);
+
+	while(1){
+
+		FD_SET(sd, &rfds);
+
+		if (select(nfds, &rfds, (fd_set *)0, (fd_set *)0,(struct timeval *)0) < 0){
+			printf("select() Failure\n");
+	    	exit(1);
+		}
+
+		if(FD_ISSET(sd,&rfds))
+		{
+
+			csock=accept(sd,(struct sockaddr *) &client_addr,&clilength);
+
+			if(csock<0)
+				if (errno == EINTR)
+						continue;
+
+			printf("%s client with port number %d is connected to  server %s with port number %d \n",inet_ntoa(client_addr.sin_addr),htons(client_addr.sin_port),inet_ntoa(server_addr.sin_addr),htons(server_addr.sin_port));
+			pthread_create(&thread1, NULL, (void *(*)(void*))client_req, (void *)&csock);
+		}
+
+	}
+
+	pthread_join(thread1,NULL);
+	close(sd);
+
+	return 0;
+}
+
+void *client_req(void* arg)
+{
+	int read_flag=0;
+	char buff[256];
+	struct sockaddr_in client_addr, server_addr;
+	struct stat obj;
+	int filehandle,size;
+	FILE *file ;
+	FILE *newFile;
+
+	int csock = *(int*)arg;
+
+	while(1){
+
+		bzero(buff,sizeof(buff));
+		read_flag=read(csock,buff,sizeof(buff));
+
+		if(read_flag<0){
+			printf("Error in reading\n");
+		}
+
+		if(!strcmp(buff, "quit"))
+		{
+		  int i = 1;
+		  send(csock, &i, sizeof(int), 0);
+		}
+
+		if(!strncmp(buff,"usage",5))
+		{
+			char *token;
+			token=strtok(buff,"|");
+			int flag=0,client_id=0, water_val=0,gas_val=0,elec_val=0;
+			char time_stamp[200],cid[20];
+			bzero(time_stamp,200);
+
+			 while(token!=NULL)
+			 {
+				 if(1==flag)
+				 {
+					 water_val=atoi(token);
+				 }
+				 else if(2==flag)
+				 {
+					 gas_val= atoi(token);
+				 }
+				 else if(3==flag)
+				 {
+					 elec_val= atoi(token);
+				 }
+				 else if(4==flag)
+				 {
+					 strcpy(time_stamp,token);
+				 }
+				 else if(5==flag)
+				 {
+					 client_id= atoi(token);
+					 strcpy(cid,token);
+
+				 }
+				 flag++;
+				 token=strtok(NULL,"|");
+			 }
+
+			char str[20];
+			strcpy(str,"User ");
+			strcat(str,cid);
+			strcat(str,".txt");
+			file = fopen(str,"a+");
+
+			if (file == NULL)
+			{
+			  printf("Error opening file!\n");
+			  exit(1);
+			}
+
+			fscanf(file,"%s",firstname);
+
+			if(!strncmp(firstname,"water",5)) {
+				fprintf(file,"%d\t%d\t%d\t\t%s\t\t%d\n",water_val,gas_val,elec_val,time_stamp,client_id);
+			}else{
+				fprintf(file,"water\tgas\telectricity\ttime_day\t\t\tclient_id\n");
+				fprintf(file,"%d\t%d\t%d\t\t%s\t\t%d\n",water_val,gas_val,elec_val,time_stamp,client_id);
+			}
+
+			bzero(firstname,sizeof(firstname));
+			bzero(buff,sizeof(buff));
+
+			pthread_mutex_lock(&usage_lock);
+
+			sprintf(buff,"-----------------------------------------------------------------\n");
+
+			if(water_val > w_val) {
+				sprintf(buff, "%sYou have crossed the threshold for water by %0.2f%%\n",buff,consumption(water_val,w_val));
+			} else if(water_val < w_val){
+				sprintf(buff, "%sCongratulations! You have conserved %0.2f%% water\n",buff,conservation(water_val,w_val));
+			} else {
+				sprintf(buff,"%sYour current water usage is equal to threshold\n",buff);
+			}
+
+			if(gas_val > g_val) {
+				sprintf(buff, "%s\nYou have crossed the threshold for gas by %0.2f%%\n",buff,consumption(gas_val,g_val));
+			} else if(gas_val < g_val){
+				sprintf(buff, "%s\nCongratulations! You have conserved %0.2f%% gas\n",buff,conservation(gas_val,g_val));
+			} else {
+				sprintf(buff,"%s\nYour current gas usage is equal to threshold\n",buff);
+			}
+
+			if(elec_val > e_val) {
+				sprintf(buff, "%s\nYou have crossed the threshold for electricity by %0.2f%%\n",buff,consumption(elec_val,e_val));
+			} else if(elec_val < e_val){
+				sprintf(buff, "%s\nCongratulations! You have conserved %0.2f%% electricity\n",buff,conservation(elec_val,e_val));
+			} else {
+				sprintf(buff,"%s\nYour current electricity usage is equal to threshold\n",buff);
+			}
+
+			pthread_mutex_unlock(&usage_lock);
+
+			write(csock,buff,strlen(buff));
+
+			fclose(file);
+		}
+
+		if(strncmp(buff,"text",4)==0)
+		{
+			char *token;
+			token=strtok(buff,"|");
+			int flag=0;
+			char cid[20];
+
+			while(token!=NULL)
+			{
+				if(flag==1)
+				 {
+					 strcpy(cid,token);
+				 }
+			 flag++;
+			 token=strtok(NULL,"|");
+			}
+
+			char str[20];
+			strcpy(str,"User ");
+			strcat(str,cid);
+			strcat(str,".txt");
+
+			file=fopen(str,"r");
+
+			if (file == NULL)
+			{
+			  printf("Error opening file!\n");
+			  exit(1);
+			}
+
+			char str1[30] = {0};
+			strcpy(str1,"User_");
+			strcat(str1,cid);
+			strcat(str1,".txt");
+
+			strcpy(buff,str1);
+			write(csock,buff,strlen(buff));
+
+			read(csock,buff,sizeof(buff));
+
+			char ch[150] = {0};
+
+			newFile = fopen(str1, "w");
+
+			if (newFile == NULL)
+			{
+				printf("Error opening file!\n");
+				exit(1);
+			}
+
+			while (fgets(ch, 150, file) != NULL)
+			{
+				fputs(ch,newFile);
+			}
+
+			fclose(newFile);
+			fclose(file);
+
+			stat(str1, &obj);
+			filehandle = open(str1, O_RDONLY);
+			size = obj.st_size;
+
+			if(filehandle == -1)
+			  size = 0;
+			send(csock, &size, sizeof(int), 0);
+			if(size)
+			  sendfile(csock, filehandle, NULL, size);
+		}
+
+		if(strncmp(buff,"graph",5)==0)
+		{
+			char *token;
+			token=strtok(buff,"|");
+			int flag=0,client_id=0;
+			char cid[20];
+
+			 while(token!=NULL)
+			 {
+				if(1==flag)
+				 {
+					 client_id= atoi(token);
+					 strcpy(cid,token);
+				 }
+				 flag++;
+				 token=strtok(NULL,"|");
+			 }
+
+			 char str[20];
+			 strcpy(str,"User ");
+			 strcat(str,cid);
+			 strcat(str,".txt");
+
+			 char str1[30] = {0};
+			 strcpy(str1,"User_");
+			 strcat(str1,cid);
+			 strcat(str1,".png");
+
+			 strcpy(buff,str1);
+			 write(csock,buff,strlen(buff));
+
+			 read(csock,buff,sizeof(buff));
+			 char timefmt[]="%m/%d/%Y %H:%M:%S";
+			 gnuplot_ctrl * g = gnuplot_init();
+			 gnuplot_cmd(g, "set terminal png");
+			 gnuplot_cmd(g, "set output \"%s\"",str1);
+			 gnuplot_cmd(g,"set xdata time");
+			 gnuplot_cmd(g,"set timefmt \"%s\"",timefmt);
+			 gnuplot_cmd(g,"set format x \"%s\"",timefmt);
+			 gnuplot_cmd(g, "set xtics rotate # crucial line");
+			 gnuplot_cmd(g, "set xlabel \"Duration\"");
+			 gnuplot_cmd(g, "set ylabel \"Usage\"");
+			 gnuplot_cmd(g, "set yrange [0:500]");
+			 gnuplot_cmd(g, "plot \"%s\" using 4:1 with linespoints title columnhead,\\",str);
+			 gnuplot_cmd(g, "\"%s\" using 4:2 with linespoints title columnhead,\\",str);
+			 gnuplot_cmd(g, "\"%s\" using 4:3 with linespoints title columnhead",str);
+
+			 gnuplot_close(g);
+
+			 bzero(buff,256);
+			 strcpy(buff," Graph Report Generated");
+
+			 stat(str1, &obj);
+			 filehandle = open(str1, O_RDONLY);
+			 size = obj.st_size;
+
+			 if(filehandle == -1)
+			   size = 0;
+			 send(csock, &size, sizeof(int), 0);
+
+			 if(size)
+			  sendfile(csock, filehandle, NULL, size);
+
+		}
+	}
+
+}
+
+float conservation(int value,int threshold) {
+
+	float computedValue = threshold-value;
+	computedValue = computedValue/threshold;
+	computedValue = computedValue*100;
+	return computedValue;
+
+}
+
+float consumption(int value,int threshold) {
+
+	float computedValue = value-threshold;
+	computedValue = computedValue/threshold;
+	computedValue = computedValue*100;
+	return computedValue;
+
+}
